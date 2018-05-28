6818c2e1a56561529a7673ee7a1cd7e70935b997
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-28 23:28:05 +0200

    Tried some new consumer stuff (not working yet)

--- save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/NewImageHandler.kt
+++ save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/NewImageHandler.kt
@@ -1,12 +1,14 @@
 package com.goosvandenbekerom.shutter.saveservice
 
+import com.goosvandenbekerom.shutter.saveservice.config.KafkaConfig
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 class NewImageHandler {
-    @KafkaListener(topics = ["image"])
-    fun newImage(data: Any) {
+    @KafkaListener(topics = [KafkaConfig.TOPIC_NEW_IMAGE], containerFactory = "kafkaListenerContainerFactory")
+    fun newImage(data: ConsumerRecord<String, ByteArray>) {
         println(data)
     }
 }
--- save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/config/KafkaConfig.kt
+++ save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/config/KafkaConfig.kt
@@ -2,10 +2,7 @@ package com.goosvandenbekerom.shutter.saveservice.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.IntegerDeserializer
-import org.apache.kafka.common.serialization.IntegerSerializer
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,35 +15,25 @@ import org.springframework.kafka.core.KafkaTemplate
 @Configuration
 @EnableKafka
 class KafkaConfig {
+    companion object {
+        const val TOPIC_NEW_IMAGE = "new-image"
+    }
+
     @Value("\${spring.kafka.bootstrap-servers}")
     private lateinit var bootstrapServers: String
 
-    // Producer
-    @Bean
-    fun producerConfigs() = mapOf(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to IntegerSerializer::class,
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to StringSerializer::class,
-            ProducerConfig.MAX_BLOCK_MS_CONFIG to 5000
-    )
-    @Bean
-    fun producerFactory() = DefaultKafkaProducerFactory<Int, String>(producerConfigs())
-    @Bean
-    fun kafkaTemplate() = KafkaTemplate<Int, String>(producerFactory())
-
-    // Consumer
     @Bean
     fun consumerConfigs() = mapOf(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to IntegerDeserializer::class,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
             ConsumerConfig.GROUP_ID_CONFIG to "save-consumer"
     )
     @Bean
-    fun consumerFactory() = DefaultKafkaConsumerFactory<Int, String>(consumerConfigs())
+    fun consumerFactory() = DefaultKafkaConsumerFactory<String, ByteArray>(consumerConfigs())
     @Bean
-    fun listenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<Int, String> {
-        val factory = ConcurrentKafkaListenerContainerFactory<Int, String>()
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, ByteArray> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, ByteArray>()
         factory.consumerFactory = consumerFactory()
         return factory
     }
