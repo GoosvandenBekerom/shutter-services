2eab90e52d967edb9be7caa3fbea1bc3866d15a5
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-03 20:33:41 +0200

    Added ftp upload for received image

--- save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/NewImageHandler.kt
+++ save-service/src/main/kotlin/com/goosvandenbekerom/shutter/saveservice/NewImageHandler.kt
@@ -1,14 +1,21 @@
 package com.goosvandenbekerom.shutter.saveservice
 
 import com.goosvandenbekerom.shutter.saveservice.config.KafkaConfig
+import com.goosvandenbekerom.shutter.saveservice.services.FtpService
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
-class NewImageHandler {
+class NewImageHandler(private val ftp: FtpService) {
     @KafkaListener(topics = [KafkaConfig.TOPIC_NEW_IMAGE], containerFactory = "kafkaListenerContainerFactory")
     fun newImage(data: ConsumerRecord<String, ByteArray>) {
-        println(data)
+        val id = data.key()
+        println("received image with id $id")
+
+        if (ftp.upload(id, data.value()))
+            println("upload of image $id successful")
+        else
+            println("upload of image $id failed") // TODO: upload error to some kind of logging topic?
     }
 }
